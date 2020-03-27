02f0801ad0aa33e88cca247a4d3c3b4779d744f6
Scott Dover <scott.dover@researchsquare.com>
2020-03-27 15:50:55 -0400

    Initial commit

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const config = require('./config');
-const merge = require('webpack-merge');
-
-module.exports = {
-    copyFiles: files => new CopyWebpackPlugin(files),
-    extendDefaultWebpackConfiguration: originalConfig => merge(config, originalConfig),
-};

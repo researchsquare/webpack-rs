ef1eb48e903c99e84978b705ad775b2ebe56c1de
Scott Dover <scott.dover@researchsquare.com>
2021-11-23 15:36:15 -0500

    Remove imagemin

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const config = require('./config.js');
-const helperFunctions = require('./helpers.js');
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 const { merge } = require('webpack-merge');
 
-const helpers = helperFunctions;
 const extendDefaultWebpackConfiguration = originalConfig => merge(config, originalConfig);
 const Plugins = {
     CopyWebpackPlugin,
@@ -15,6 +13,5 @@ const Plugins = {
 
 module.exports = {
     extendDefaultWebpackConfiguration,
-    helpers,
     Plugins,
 };
